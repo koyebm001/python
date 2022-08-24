8f9b7fa701063d2f2767e2af81c3e4068bb7352f
koyebm001 <111969940+koyebm001@users.noreply.github.com>
2022-08-24 22:27:33 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,2 @@
+TOKEN="2770cf1f5e25d10628f7e6f1fffe51895663016fd1a85a8268" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+~/.buildkite-agent/bin/buildkite-agent start
